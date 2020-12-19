c837809dc15b6119b8ee01186a52f7b7d7ac3cad
mariam-ts <tsirekidze.ma@gmail.com>
2020-12-19 17:02:27 +0400

    finished

---
+++ Adress sort/Address/Address/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//you will need to change this to match the name of your project in visual studio
+
+/// <summary>
+/// mariam tsirekidze
+/// ID: 823460489
+/// Your description of the class here
+/// </summary>
+namespace ConsoleApplication
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Address : IComparable<Address>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AddressLineOne { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AddressLineTwo { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Zip { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string State { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime BirthDay { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Address()
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="addressLineOne"></param>
+        /// <param name="addressLineTwo"></param>
+        /// <param name="city"></param>
+        /// <param name="state"></param>
+        /// <param name="zip"></param>
+        /// <param name="birthDay"></param>
+        /// <param name="phoneNumber"></param>
+        public Address(string firstName, string lastName, string addressLineOne, string addressLineTwo, string city, string state, int zip, DateTime birthDay, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(Address other)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
+
